cc5f3018600a8272ebb0bdd032663737dd0c3fe6
jiaming5 <jiaming5@leju.com>
2018-08-20 15:54:20 +0800

    添加view及示例

---
+++ src/Modules/Home/Views/Index/View.php
@@ -0,0 +1,4 @@
+<h1>This is a test template!</h1>
+$a = <?php echo $a; ?>;<br />
+$b = <?php echo $b; ?>;<br />
+$data = <?php var_export($data); ?>;<br />
--- src/routes.php
+++ src/routes.php
@@ -10,11 +10,17 @@ $app->get('/', function($request, $response) {
 $app->get('/hello[/{name}]', '\Application\\Modules\\Home\\Controllers\\Index:hello');
 
 $app->group('',function() {
-    $slim = $this;
-    $this->map(['GET','POST'], '[/{controller}[/{action}]]', function($req, $res, $args) use ($slim) {
+    $container = $this->getContainer();;
+    $this->map(['GET','POST'], '[/{controller}[/{action}]]', function($req, $res, $args) use ($container) {
         $controller_name = '\Application\\Modules\\Home\\Controllers\\';
         $controller_name .= isset($args['controller']) ? ucwords($args['controller']) : 'Index';
-        $controller = new $controller_name($slim->getContainer());
+        $controller = new $controller_name($container);
+        
+        $container['view'] = function($c) {
+            $view_path = __DIR__ . '/Modules/Home/Views';
+            return new \Slim\Views\PhpRenderer($view_path);
+        };
+
        	return $controller($req, $res, $args);
     });
 });
