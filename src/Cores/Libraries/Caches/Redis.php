71e83cbbecbf3edcdbb2305ecc97f8ad8b6ad872
TimeFinger <hewn2011@126.com>
2018-09-20 06:59:50 +0800

    完善redis类

--- src/Cores/Libraries/Caches/Redis.php
+++ src/Cores/Libraries/Caches/Redis.php
@@ -10,6 +10,7 @@ class Redis
     use SingletonTrait;
 
     public static $setting = [];
+    private $client = null;
 
     public function getConnection()
     {
@@ -17,8 +18,21 @@ class Redis
         if (empty($setting)) {
             throw new \Exception('Setting is empty!');
         }
-        return SingletonRegister::getInstance()->get($key, function () use ($setting) {
-            return new RedisCache(new RedisClient($setting));
+        $client = $this->client = new RedisClient($setting);
+        return SingletonRegister::getInstance()->get($key, function () use ($client) {
+            return new RedisCache($client);
         });
+    }
+
+    public function __call($method, $params)
+    {
+        $conn = $this->getConnection();
+        if (method_exists($conn, $name)) {
+            return $conn->$name(...$params);
+        }
+        if (method_exists($this->client, $name)) {
+            return $this->client->$name(...$params);
+        }
+        throw new \Exception('Method '.$method.' not found!');
     }
 }
