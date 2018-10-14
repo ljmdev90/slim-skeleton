6b6ef13f5d9d0a57d12d0c6e5ac8f85f8466a82e
TimeFinger <hewn2011@126.com>
2018-10-14 22:16:59 +0800

    完善swoole server,支持post、get传值, 支持接收cookie, 文件上传(postman测试没成功)

--- public/server.php
+++ public/server.php
@@ -9,10 +9,17 @@ $serv->on('WorkerStart', function () {
 });
 
 $serv->on('request', function ($request, $response) {
+    // 设置$_SERVER，使slim能够读取到其中的值
     foreach ($request->server as $key => $val) {
         // 如果不指定以下代码，$_SERVER中无REQUEST_METHOD等项，导致method not allow
         $_SERVER[strtoupper($key)] = $val;
     };
+
+    // 设置$_GET、$_POST、$_COOKIE、$_FILES
+    $_GET = $request->get;
+    $_POST = $request->post;
+    $_COOKIE = $request->cookie;
+    $_FILES = $request->files;  // 不知道为什么,这里postman测试一直没值
 
     ob_start();
     $env = __DIR__ . '/../.env';
