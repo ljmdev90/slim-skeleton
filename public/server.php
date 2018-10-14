a1c51b75594007534744a3ecc181080018d504f1
TimeFinger <hewn2011@126.com>
2018-10-14 15:25:38 +0800

    初步实现swoole server,待完善

---
+++ public/server.php
@@ -0,0 +1,34 @@
+<?php
+
+$serv = new Swoole\Http\Server('0.0.0.0', 9501);
+$serv->on('WorkerStart', function () {
+    require __DIR__ . '/../vendor/autoload.php';
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
+});
+
+$serv->on('request', function ($request, $response) {
+    foreach ($request->server as $key => $val) {
+        // 如果不指定以下代码，$_SERVER中无REQUEST_METHOD等项，导致method not allow
+        $_SERVER[strtoupper($key)] = $val;
+    };
+
+    ob_start();
+    $env = __DIR__ . '/../.env';
+    if (file_exists($env)) {
+        $dotenv = new Dotenv\Dotenv(dirname($env));
+        $dotenv->load();
+    }
+    $settings = require __DIR__ . '/../src/settings.php';
+    $app = new \Slim\App($settings);
+    require __DIR__ . '/../src/dependencies.php';
+    require __DIR__ . '/../src/middleware.php';
+    require __DIR__ . '/../src/routes.php';
+    $app->run();
+    $result = ob_get_contents();
+    ob_get_clean();
+    $response->end($result);
+});
+
+$serv->start();
