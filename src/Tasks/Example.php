c6b5d2d4590c29a5414e75db08193b8b5ae48c88
TimeFinger <hewn2011@126.com>
2018-10-24 00:48:13 +0800

    初步实现命令行任务执行

---
+++ src/Tasks/Example.php
@@ -0,0 +1,12 @@
+<?php
+namespace Application\Tasks;
+
+use Application\Cores\Controller;
+
+class Example extends Controller
+{
+    public function run()
+    {
+        echo 'running...' . PHP_EOL;
+    }
+}
--- src/middleware.php
+++ src/middleware.php
@@ -1,1 +1,34 @@
 <?php
+namespace Application;
+
+class TaskMiddleware
+{
+    private $container = null;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    public function __invoke($request, $response, $next)
+    {
+        if (PHP_SAPI != 'cli') {
+            $response = $next($request, $response);
+        }
+
+        global $argv;
+
+        if (count($argv) > 1) {
+            $command = $argv[1];
+            $args = array_slice($argv, 2);
+        }
+
+        $class = '\\Application\\Tasks\\' . $command;
+        $example = new $class($this->container);
+        $example->run();
+
+        return $response;
+    }
+}
+
+$app->add(TaskMiddleware::class);
