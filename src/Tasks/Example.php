de27aed0ab372a733ccee2a24e65150a2a14ddc9
TimeFinger <hewn2011@126.com>
2018-11-28 23:37:56 +0800

    优化Task例子,添加数据库基础操作基本完成

--- src/Tasks/Example.php
+++ src/Tasks/Example.php
@@ -22,9 +22,34 @@ class Example extends Task
             if ($choice == 'initdb') {
                 $db = $this->get('db');
 
-                // todo 创建初始数据
+                // 创建测试数据库
+                $table = 'ssdb';
+                $pdostat = $db->query('CREATE DATABASE IF NOT EXISTS ' . $table . ' CHARSET utf8');
+                $this->errorCheck($pdostat);
+                $db->query('USE ' . $table);
+                // 创建测试表
+                $sql = 'CREATE TABLE IF NOT EXISTS `users`(
+                    `id` INT UNSIGNED AUTO_INCREMENT,
+                    `name` VARCHAR(30) NOT NULL DEFAULT \'\',
+                    `add_time` INT UNSIGNED NOT NULL,
+                    PRIMARY KEY (`id`)
+                )';
+                $pdostat = $db->query($sql);
+                $this->errorCheck($pdostat);
+
+                $this->line('created!');
+                break;
             }
         }
-        $this->line('Running...');
+        $this->line('Bye.');
+    }
+
+    private function errorCheck($pdostat)
+    {
+        $error = $pdostat->errorInfo();
+        if ($error[0] !== '00000') {
+            $this->line($error[2]);
+            exit;
+        }
     }
 }
