ffa90939bd6a0d26b46c29cbda89c770e50d8ea6
TimeFinger <hewn2011@126.com>
2018-12-07 23:48:20 +0800

    为控制器基类添加相关注释

--- src/Cores/Controller.php
+++ src/Cores/Controller.php
@@ -1,6 +1,9 @@
 <?php
 namespace Application\Cores;
 
+/**
+ * 控制器基类
+ */
 abstract class Controller
 {
     protected $container;
@@ -8,12 +11,20 @@ abstract class Controller
     public function __construct($container)
     {
         $this->container = $container;
+        
+        // 设置Model类的$container,以便Model中能方便的读取诸如系统配置等内容
         Model::setContainer($container);
+        
+        // 方便子类初始化,防止子类直接调用构造方法
         if (method_exists($this, 'init')) {
             $this->init();
         }
     }
 
+    /**
+     * 当以方法的方式调用对象时,调用该方法
+     * 用于统一处理标准url格式(如:/Controller/Action)的请求
+     */
     public function __invoke($req, $res, $args)
     {
         $actions = explode('.', isset($args['action']) ? $args['action'] : 'index');
@@ -30,11 +41,17 @@ abstract class Controller
         throw new NotFoundException($req, $res);
     }
     
+    /**
+     * 封装container类的get方法,方便调用
+     */
     protected function get($name)
     {
         return $this->container->get($name);
     }
 
+    /**
+     * 渲染模板
+     */
     protected function render($data = array(), $template_file = '')
     {
         if (empty($template_file)) {
