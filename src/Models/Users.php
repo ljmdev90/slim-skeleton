85a4889c5ec94a23955ce0474eb75e78e1b0d750
TimeFinger <hewn2011@126.com>
2019-01-07 23:37:27 +0800

    添加注释

--- src/Models/Users.php
+++ src/Models/Users.php
@@ -3,6 +3,9 @@ namespace Application\Models;
 
 use Application\Cores\Model;
 
+/**
+ * Model示例
+ */
 class Users extends Model
 {
     public static $table = 'users';
--- src/Modules/Home/Models/Users.php
+++ src/Modules/Home/Models/Users.php
@@ -3,6 +3,9 @@ namespace Application\Modules\Home\Models;
 
 use Application\Cores\Model;
 
+/**
+ * Model示例, 同src/Models/Users.php 一样
+ */
 class Users extends Model
 {
     public static $table = 'users';
