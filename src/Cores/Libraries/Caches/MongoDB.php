cbfa540a6ab762744594ee3b22d5cf73dd9b45b8
TimeFinger <hewn2011@126.com>
2019-05-08 00:25:51 +0800

    完善mongodb类(待验证)

--- src/Cores/Libraries/Caches/MongoDB.php
+++ src/Cores/Libraries/Caches/MongoDB.php
@@ -1,7 +1,27 @@
 <?php
 namespace Application\Cores\Libraries\Caches;
 
+use MongoDB\Client;
+
 class MongoDB
 {
-    
+    public static $setting = [];
+    private $client = null;
+
+    public function __construct($configs = [])
+    {
+        if (empty($configs)) {
+            throw new \Exception('configs is empty!');
+        }
+        $uri = $configs['uri'] ?? 'mongodb://127.0.0.1/';
+        $this->client = new Client($uri);
+    }
+
+    public function __call($method, $params)
+    {
+        if (method_exists($this->client, $method)) {
+            return $this->client->$method(...$params);
+        }
+        throw new \Exception('Method \'' . $method . '\' not found!');
+    }
 }
